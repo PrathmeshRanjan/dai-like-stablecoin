3203b1b9a00e3f8335f3ddbc6fb598a8e516b7fb
PrathmeshRanjan <prathmeshranjan@gmail.com>
2025-10-05 16:37:27 +0530

    second commit

--- src/SCEngine.sol
+++ src/SCEngine.sol
@@ -49,7 +49,8 @@ contract SCEngine is ReentrancyGuard {
     uint256 private constant _LIQUIDATION_THRESHOLD = 50; // 200% over-collateralized
     uint256 private constant _LIQUIDATION_PRECISION = 100;
     uint256 private constant _PRECISION = 1e18;
-    uint256 private constant _MIN_HEALTH_FACTOR = 1;
+    uint256 private constant _MIN_HEALTH_FACTOR = 1e18;
+    uint256 private constant _LIQUIDATION_BONUS = 10; // This means you get assets at a 10% discount when liquidating
 
     Stablecoin immutable _I_SC;
 
@@ -57,7 +58,7 @@ contract SCEngine is ReentrancyGuard {
     //// EVENTS ///////
     ///////////////////
     event CollateralDeposited(address indexed user, address indexed token, uint256 amountDeposited);
-    event CollateralRedeemed(address indexed user, address indexed token, uint256 amountRedeemed);
+    event CollateralRedeemed(address indexed from, address indexed to, address indexed token, uint256 amountRedeemed);
 
     ///////////////////
     //// ERRORS ///////
@@ -67,6 +68,8 @@ contract SCEngine is ReentrancyGuard {
     error SCEngine__TransferFailed();
     error SCEngine__HealthFactorIsBelowMinimum(uint256 healthFactor);
     error SCEngine__MintFailed();
+    error SCEngine__CannotLiquidatePosition();
+    error SCEngine__HealthFactorNotImproved();
 
     ///////////////////
     //// MODIFIERS ////
@@ -153,23 +156,96 @@ contract SCEngine is ReentrancyGuard {
      * @notice If you have SC minted, you will not be able to redeem until you burn your SC
      */
     function redeemCollateral(address tokenCollateralAddress, uint256 amountCollateral)
-        external
+        public
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
         nonReentrant
     {
-        _collateralDeposited[msg.sender][tokenCollateralAddress] -= amountCollateral;
-        emit CollateralRedeemed(msg.sender, tokenCollateralAddress, amountCollateral);
-        bool success = IERC20(tokenCollateralAddress).transfer(msg.sender, amountCollateral);
-        if(!success) {
-            revert SCEngine__TransferFailed();
-        }
+        _redeemCollateral(tokenCollateralAddress, amountCollateral, msg.sender, msg.sender);
         _revertIfHealthFactorIsBroken(msg.sender); // This will revert all the previous steps if the health factor breaks.
+    }
+
+    /*
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're withdrawing
+     * @param amountCollateral: The amount of collateral you're withdrawing
+     * @param amountScToBurn: The amount of SC you want to burn
+     * @notice This function will withdraw your collateral and burn SC in one transaction
+     */
+    function redeemCollateralForSc(address tokenCollateralAddress, uint256 amountCollateral) external {
+        burnSc(amountCollateral);
+        redeemCollateral(tokenCollateralAddress, amountCollateral);
+    }
+
+    function burnSc(uint256 amount) public moreThanZero(amount) nonReentrant {
+        _burnSc(amount, msg.sender, msg.sender);
+    }
+
+    /*
+     * @param collateral: The ERC20 token address of the collateral you're using to make the protocol solvent again.
+     * This is collateral that you're going to take from the user who is insolvent.
+     * In return, you have to burn your DSC to pay off their debt, but you don't pay off your own.
+     * @param user: The user who is insolvent. They have to have a _healthFactor below MIN_HEALTH_FACTOR
+     * @param debtToCover: The amount of DSC you want to burn to cover the user's debt.
+     *
+     * @notice: You can partially liquidate a user.
+     * @notice: You will get a 10% LIQUIDATION_BONUS for taking the users funds.
+    * @notice: This function working assumes that the protocol will be roughly 150% overcollateralized in order for this
+    to work.
+    * @notice: A known bug would be if the protocol was only 100% collateralized, we wouldn't be able to liquidate
+    anyone.
+     * For example, if the price of the collateral plummeted before anyone could be liquidated.
+     */
+    function liquidate(address collateral, address user, uint256 debtToCover)
+        external
+        isAllowedToken(collateral)
+        moreThanZero(debtToCover)
+        nonReentrant
+    {
+        uint256 startingUserHealthFactor = _healthFactor(user);
+        if (startingUserHealthFactor >= _MIN_HEALTH_FACTOR) {
+            revert SCEngine__CannotLiquidatePosition();
+        }
+        // If covering 100 SC, we need to $100 of collateral
+        uint256 tokenAmountFromDebtCovered = getTokenAmountFromUsd(collateral, debtToCover);
+        // And give them a 10% bonus
+        // So we are giving the liquidator $110 of WETH for 100 SC
+        uint256 bonusCollateral = (tokenAmountFromDebtCovered * _LIQUIDATION_BONUS) / _LIQUIDATION_PRECISION;
+        // Burn SC equal to debtToCover
+        // Figure out how much collateral to recover based on how much burnt
+        _redeemCollateral(collateral, tokenAmountFromDebtCovered + bonusCollateral, user, msg.sender);
+        _burnSc(debtToCover, user, msg.sender);
+
+        uint256 endingUserHealthFactor = _healthFactor(user);
+        // This conditional should never hit, but just in case
+        if (endingUserHealthFactor <= startingUserHealthFactor) {
+            revert SCEngine__HealthFactorNotImproved();
+        }
+        _revertIfHealthFactorIsBroken(msg.sender);
     }
 
     ///////////////////////////////////////////
     //// PRIVATE & INTERNAL VIEW FUNCTIONS ////
     ///////////////////////////////////////////
+    function _burnSc(uint256 amountScToBurn, address onBehalfOf, address scFrom) internal {
+        _scMinted[onBehalfOf] -= amountScToBurn;
+        _I_SC.transferFrom(scFrom, address(this), amountScToBurn);
+        _I_SC.burn(amountScToBurn);
+    }
+
+    function _redeemCollateral(address tokenCollateralAddress, uint256 amountCollateral, address from, address to)
+        public
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        _collateralDeposited[msg.sender][tokenCollateralAddress] -= amountCollateral;
+        emit CollateralRedeemed(from, to, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transfer(to, amountCollateral);
+        if (!success) {
+            revert SCEngine__TransferFailed();
+        }
+    }
+
     function _getAccountInformation(address user)
         private
         view
@@ -206,5 +282,15 @@ contract SCEngine is ReentrancyGuard {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(_priceFeeds[token]);
         (, int256 price,,,) = priceFeed.latestRoundData();
         return ((uint256(price) * _ADDITIONAL_FEED_PRECISION) * amount) / _PRECISION; // ETH/USD and BTC/USD price feed returns value with 8 decimal places
+    }
+
+    function getTokenAmountFromUsd(address token, uint256 usdAmountInWei) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(_priceFeeds[token]);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // $100e18 USD Debt
+        // 1 ETH = 2000 USD
+        // The returned value from Chainlink will be 2000 * 1e8
+        // Most USD pairs have 8 decimals, so we will just pretend they all do
+        return ((usdAmountInWei * _PRECISION) / (uint256(price) * _ADDITIONAL_FEED_PRECISION));
     }
 }
